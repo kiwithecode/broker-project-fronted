a9d9bba63f9f5c36328ee96275cb14e6244679af
kiwithecode <wowcelodion@gmail.com>
2024-05-31 19:50:52 -0500

    broker estruc

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
-//import App from './App.vue';
-//import router from './router';
-//import store from './store'
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import './styles/tailwind.css'; // Importar Tailwind CSS
 
-//createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
